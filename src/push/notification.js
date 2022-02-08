f66434f7aea9f852b8bcee1b8cffa1535410d1f0
martinheitmann <mart.hei@live.com>
2022-02-08 13:35:42 +0100

    Remove invalid fcm token on error

--- src/push/notification.js
+++ src/push/notification.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
 const logger = require("../util/logger");
+const User = require("../model/user/user");
 
 const tag = "notification: ";
 
@@ -44,7 +45,7 @@ module.exports.sendNormalPriorityNotification = async function (token, data) {
     }
     return null;
   } catch (err) {
-    errorLogger(err);
+    handleFcmError(err, token);
     throw err;
   }
 };
@@ -71,7 +72,7 @@ module.exports.sendHighPriorityNotification = async function (token, data) {
     }
     return null;
   } catch (err) {
-    errorLogger(err);
+    handleFcmError(err, token);
     throw err;
   }
 };
@@ -101,10 +102,44 @@ module.exports.sendDataNotification = async function (priority, token, data) {
         throw new Error("Invalid result from fcm messaging.");
       }
     } catch (err) {
-      errorLogger(err);
+      handleFcmError(err, token);
       throw err;
     }
   } else {
     throw new Error("Priority must be either HIGH or NORMAL");
   }
 };
+
+function handleFcmError(error, token) {
+  errorLogger(error);
+  if (error.code) {
+    // Should probably also handle other FCM errors, but other errors may have multiple causes.
+    // Probably safe to remove the token for this error code.
+    if (error.code === "messaging/registration-token-not-registered") {
+      removeFCMTokens(token);
+    }
+  }
+}
+
+async function removeFCMTokens(fcmToken) {
+  // In theory fine to just look for one user, but if multiple of the same
+  // token exists we can might as well clear those too.
+  const users = await User.findUsers({ fcmRegistrationToken: fcmToken });
+  if (users && users.length > 0) {
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      const userId = user._id;
+      if (userId) {
+        const updatedUser = await User.updateUser(userId, {
+          fcmRegistrationToken: null,
+        });
+        if (updatedUser) {
+          logger.log({
+            level: "info",
+            message: tag + "removed invalid fcm token for " + updatedUser._id,
+          });
+        }
+      }
+    }
+  }
+}
